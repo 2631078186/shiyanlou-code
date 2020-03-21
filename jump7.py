c47572010c8edbffb7278fed97e38a7baa13ba43
2631078186 <2631078186@qq.com>
2020-03-22 05:17:52 +0800

    5

---
+++ jump7.py
@@ -0,0 +1,6 @@
+i = 0 
+while i < 100:
+    i = i + 1
+    if (i % 7==0)or(i==17)or(i==37)or(i==47)or(i==57)or(i==67)or(i==87)or(i==97)or(i==27)or(i==71)or(i==72)or(i==73)or(i==74)or(i==75)or(i==76)or(i==78)or(i==79):    
+       continue
+    print(i)    
